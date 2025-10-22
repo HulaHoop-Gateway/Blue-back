3a3dfd8c754916cebc31dec347f8f16505c2cb19
smin0111 <seungmin6385@gmail.com>
2025-10-22 14:37:19 +0900

    feature-blueback-lsm-1022

---
+++ src/main/java/com/hulahoop/blueback/BlueBackApplication.java
@@ -0,0 +1,13 @@
+package com.hulahoop.blueback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlueBackApplication.class, args);
+    }
+
+}
